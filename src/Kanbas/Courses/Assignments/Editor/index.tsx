945210f455db16be4b3a58707769997b6fb14a5f
Pranjal Rane <rane.pr@northeastern.edu>
2024-03-23 18:13:52 -0400

    add assignments page and edit assignments page

--- src/Kanbas/Courses/Assignments/Editor/index.tsx
+++ src/Kanbas/Courses/Assignments/Editor/index.tsx
@@ -1,21 +1,151 @@
-import React from "react";
 import { useNavigate, useParams, Link } from "react-router-dom";
-import { assignments } from "../../../Database";
+import { useDispatch, useSelector } from "react-redux";
+import { setAssignment, addAssignment, updateAssignment } from "../reducer";
+import { KanbasState } from "../../../store";
+
 function AssignmentEditor() {
+  const assignmentList = useSelector((state: KanbasState) => 
+    state.assignmentsReducer.assignments);
+
+  
+  const dispatch = useDispatch();
   const { assignmentId } = useParams();
-  const assignment = assignments.find(
-    (assignment) => assignment._id === assignmentId);
   const { courseId } = useParams();
   const navigate = useNavigate();
+  var assignment = useSelector((state: KanbasState) => 
+  state.assignmentsReducer.assignment);
+
+  if (assignmentId != "new") {
+    assignment =assignmentList.find((assignment) => assignment._id === assignmentId);
+
+    console.log(assignment)
+   } 
+
+
+  // const assignment = useSelector((state: KanbasState) => 
+  // state.assignmentsReducer.assignment);
+  
+
+  const aFrom = assignment.availableFromDate;
+  const aUntil = assignment.availableUntilDate;
+  
+  
   const handleSave = () => {
-    console.log("Actually saving assignment TBD in later assignments");
+    if (assignmentId === "new") {
+      
+      dispatch(addAssignment({ ...assignment, course: courseId, _id: new Date().getTime().toString()}));
+    } else {
+
+      dispatch(updateAssignment({...assignment, course: courseId}));
+    }
     navigate(`/Kanbas/Courses/${courseId}/Assignments`);
   };
+
   return (
-    <div>
-      <h2>Assignment Name</h2>
+    <div className="me-5">
+    <div className="wd-kanbas-green float-end mt-2 ">
+    <i className="fa fa-check-circle wd-kanbas-green" aria-hidden="true"></i>
+        Published
+        <button type="button" className="btn btn-light"><i className="fa fa-ellipsis-v ms-2"></i></button>
+    </div>
+    <br />
+    <br />
+    <hr />
+    <label htmlFor="input-1" className="form-label"> Assignment Name </label>
       <input value={assignment?.title}
-             className="form-control mb-2" />
+             className="form-control mb-2"  onChange={(e) => dispatch(setAssignment({...assignment,title: e.target.value}))}/>
+    <br />
+    <textarea value={assignment.description} className="form-control" onChange={(e) => dispatch(setAssignment({...assignment, description:e.target.value}))}></textarea>
+    <br />
+    <div className="container">
+        <div className="row">
+        <div className="col-2">
+            <div className="float-end">Points</div>
+        </div>
+        <div className="col-8">
+            <input className="form-control" id="input-2" value={assignment.points} onChange={(e) =>
+                dispatch(setAssignment({ ...assignment, points: e.target.value }))
+              }/>
+        </div>
+        <div className="col-2"></div>
+        </div>
+        <br />
+        <br />     
+                          <div className="row">
+                            <div className="col-2">
+                              <div className="float-end">Assign</div>
+                            </div>
+                            <div className="col-8">
+                              <ul className="list-group list-group-item wd-kanbas-edit-section">
+                                <li className="list-group-item border-0">
+                                  <b>Due</b>
+                                </li>
+                  
+                                <li className="list-group-item border-0">
+                                  <input
+                                    type="date"
+                                    className="form-control"
+                                    id="input-4"
+                                    value={assignment.dueDate}
+                                    onChange={(e) =>
+                                      dispatch(setAssignment({ ...assignment, dueDate: e.target.value }))
+                                    }
+                                  />
+                                </li>
+                                <li className="list-group-item border-0">
+                                    <div className="row">
+                                        <div className="col-6 float-start">
+                                            <b className="wd-kanbas-width-45">Available from</b>
+                                        </div>
+                                        <div className="col-6 wd-float-start">
+                                            <b className="wd-kanbas-width-45">Until</b>
+                                        </div>
+                                    </div>
+                                </li>
+        
+                                <li className="list-group-item border-0">
+                                    <div className="row">
+                                        <div className="col-6 float-start">
+                                            <input
+                                            type="date"
+                                            className="form-control float-start wd-kanbas-width-45 me-1"
+                                            id="input-5"
+                                            value={aFrom}
+                                            onChange={(e) =>
+                                              dispatch(
+                                                setAssignment({ ...assignment, availableFromDate: e.target.value })
+                                              )
+                                            }
+                                          />
+                                        </div>
+                                        <div className="col-6 float-start">
+                                            <input
+                                                type="date"
+                                                className="form-control float-start wd-kanbas-width-45 ms-1"
+                                                id="input-6"
+                                                value={aUntil}
+                                                onChange={(e) =>
+                                                  dispatch(
+                                                    setAssignment({ ...assignment, availableUntilDate: e.target.value })
+                                                  )
+                                                }
+                                            />
+                                        </div>
+                                    </div>
+                                </li>
+                              </ul>
+                            </div>
+                            <div className="col-2"></div>
+                          </div>
+                        </div>
+                  
+                        <hr />
+                  
+                        <div className="float-start">
+                          <input className="form-check-input" type="checkbox" id="check-9" />
+                          <label className="form-check-label" htmlFor="check-9"
+                            >Notify users that this content has changed</label>
+                        </div>
       <button onClick={handleSave} className="btn btn-success ms-2 float-end">
         Save
       </button>
@@ -24,6 +154,7 @@ function AssignmentEditor() {
         Cancel
       </Link>
     </div>
-  );
+
+  )
 }
 export default AssignmentEditor;
--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -4,12 +4,33 @@ import { Link, useParams } from "react-router-dom";
 import { assignments } from "../../Database";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addAssignment,
+  deleteAssignment,
+  updateAssignment,
+  setAssignment,
+} from "./reducer";
+import { KanbasState } from "../../store";
 
 function Assignments() {
   const { courseId } = useParams();
-  const assignmentList = assignments.filter(
-    (assignment) => assignment.course === courseId);
-  
+  const intialAssignment =  {
+    title: "New Assignment Title",
+    course: "Assignment's Course",
+    description: "New Description",
+    points: "100",
+    dueDate: "2023-09-18",
+    availableFromDate: "2023-09-11",
+    availableUntilDate: "2023-09-18",
+  };
+
+  const assignmentList = useSelector((state: KanbasState) => 
+    state.assignmentsReducer.assignments);
+  const assignment = useSelector((state: KanbasState) => 
+    state.assignmentsReducer.assignment);
+  const dispatch = useDispatch();
+
   const [value, setValue] = useState('');
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -25,7 +46,16 @@ function Assignments() {
         <div className="col top-button-bar d-flex justify-content-end">
           <a> Group </a>
     
-          <a className = "red" ><FaPlus /> Assignment </a>
+          {/* <a className = "red" onClick={() => dispatch(setAssignment(intialAssignment))}><FaPlus /> Assignment </a> */}
+
+          <Link to={`/Kanbas/Courses/${courseId}/Assignments/new`}>
+            <button
+              type="submit"
+              className="btn btn-danger float-end me-1 wd-kanbas-save-profile btn-danger" onClick={() => dispatch(setAssignment(intialAssignment))}>
+              <i className="fa fa-plus"></i>  
+              <span className="wd-left-margin-10" >Assignment</span>
+            </button>
+          </Link>
     
           <a> <FaEllipsisV className="me-2 custom" /> </a>
         </div>
@@ -46,8 +76,33 @@ function Assignments() {
               </span>
           </div>
           <ul className="list-group">
-            {assignmentList.map((assignment) => (
+            {/* {assignmentList.map((assignment) => (
               <li className="list-group-item">
+                <FaEllipsisV className="me-2" />
+                <FaBook className="me-2"/>
+                <Link
+                   to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}>{assignment.title}</Link>
+                <span className="float-end">
+                  <FaCheckCircle className="text-success" /><FaEllipsisV className="ms-2" /></span>
+              </li>))} */}
+
+            {assignmentList
+            .filter(
+                (assignment) => assignment.course === courseId)
+              .map((assignment) => (
+              <li className="list-group-item">
+                
+                <button
+                  onClick={() => dispatch(setAssignment(assignment))}>
+                  Edit
+                </button>
+              
+                <button
+                  onClick={() => dispatch(deleteAssignment(assignment._id))}>
+
+                  Delete
+                </button>
+
                 <FaEllipsisV className="me-2" />
                 <FaBook className="me-2"/>
                 <Link
