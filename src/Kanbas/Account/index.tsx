eeab570b23a8efd1974d6e29623e7f6fd241bcb4
Pranjal Rane <rane.pr@northeastern.edu>
2024-04-18 15:09:08 -0400

    bug fixes

--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -10,7 +10,7 @@ export default function Account() {
       <Routes>
         <Route path="/" element={<Navigate to="/Kanbas/Account/Signin" />} />
         <Route path="/Signin" element={<Signin />} />
-        <Route path="/Signin" element={<Signup />} />
+        <Route path="/Signup" element={<Signup />} />
         <Route path="/Profile" element={<Profile />} />
         <Route path="/Admin/Users" element={<UserTable />} />
       </Routes>
